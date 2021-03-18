543b1cd3d6b6a66b50217282c34596140483b630
Angel Ariel <you@example.com>
2021-03-18 20:52:23 -0300

    Ejercicio 3

---
+++ Ejercicio_3/Ejercicio_3/Ejercicio_3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Ejercicio_3
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int k = 0;
+
+            int cont = 0;
+            int resto = 0;
+
+            string cadena = " ";
+            Console.WriteLine("Ingrese un numero");
+            cadena = Console.ReadLine();
+            n = Validacion.ValidarEntero(cadena);
+
+            for (int i=1;i<=n;i++)
+            {
+                k = i;
+                for (int j=1;j<=k;j++)
+                {
+                    resto = i % j;
+                    if (resto==0)
+                    {
+                        cont = cont + 1;
+                    }
+                    if (cont > 2)
+                    {
+                        break;
+                    }
+                }
+                if (cont <= 2)
+                {
+                    Console.WriteLine("{0} es PRIMO",k);
+                    Console.ReadKey();
+                }
+                cont = 0;
+            }
+
+            
+        }
+    }
+}
---
+++ Ejercicio_3/Ejercicio_3/Validacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Validacion
+    {
+        public static int ValidarEntero(string cadena)
+        {
+            int retorno = 0;
+
+            while (!(int.TryParse(cadena, out retorno)))
+            {
+                Console.WriteLine("ERROR,..Reingrese un numero");
+                cadena = Console.ReadLine();
+            }
+            return retorno; 
+        }
+    }
+}
