85ad33369bb4d9594b25ce19ba32d2057475de57
Angel Ariel <you@example.com>
2021-03-18 21:00:55 -0300

    Ejercicio 4

---
+++ Ejercicio_4/Ejercicio_4/Ejercicio_4.cs
@@ -0,0 +1,48 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    class Ejercicio_4
+    {
+        static void Main(string[] args)
+        {
+            int resto = 0;
+            int acum = 0;
+            int n = 0;
+            int cont = 0;
+
+            for(int j=2;j<j+1;j++)
+            {
+                for (int k =1;k<j;k++)
+                {
+                    resto = j % k;
+
+                    if(resto==0)
+                    {
+                        acum = acum + k;
+                        n = j;
+                    }
+                }
+                if (acum==n)
+                {
+                    Console.WriteLine("{0} es numero perfecto",n);
+                    Console.ReadKey();
+                    j = n + 1;
+                    cont = cont + 1;
+                }
+
+                acum = 0;
+
+                if (cont==4)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
